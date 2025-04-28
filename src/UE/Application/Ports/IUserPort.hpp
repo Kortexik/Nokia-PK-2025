0aec001a3c94966e36c1b656d8c1d0aa9a2c9e6a
Krzysztof Mazur <mazurkrzysztof377@example.com~>
2025-04-28 21:54:48 +0200

    Add interface methods for sending and composing SMS

--- src/UE/Application/Ports/IUserPort.hpp
+++ src/UE/Application/Ports/IUserPort.hpp
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "Messages/PhoneNumber.hpp"
 
 namespace ue
 {
@@ -8,6 +8,7 @@ class IUserEventsHandler
 {
 public:
     virtual ~IUserEventsHandler() = default;
+    virtual void handleSendMessage(common::PhoneNumber dest, const std::string& message) = 0;
 };
 
 class IUserPort
@@ -18,6 +19,7 @@ public:
     virtual void showNotConnected() = 0;
     virtual void showConnecting() = 0;
     virtual void showConnected() = 0;
+    virtual void setSmsComposeMode() = 0;
 };
 
 }
